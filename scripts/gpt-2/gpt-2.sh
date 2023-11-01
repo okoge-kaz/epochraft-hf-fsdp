cb5a1af4730b4b3d89898c7eeb35cce70b2f4bc7
kazuki <kaz.tokyo.tech20@gmail.com>
2023-11-01 15:19:34 +0900

    feat: multi-node training

--- scripts/gpt-2/gpt-2.sh
+++ scripts/gpt-2/gpt-2.sh
@@ -16,5 +16,43 @@ module load hpcx/2.12
 cd /bb/llm/gaf51275/mistral/epochraft-hf-fsdp
 source .env/bin/activate
 
-python pretrain.py \
-  scripts/gpt-2/gpt-2-grad-shard.yaml
+# distributed settings
+export MASTER_ADDR=$(/usr/sbin/ip a show dev bond0 | grep 'inet ' | awk '{ print $2 }' | cut -d "/" -f 1)
+export MASTER_PORT=$((10000 + ($JOB_ID % 50000)))
+
+echo "MASTER_ADDR=${MASTER_ADDR}"
+
+# hostfile
+
+if [[ "$SGE_RESOURCE_TYPE" == "rt_F" ]]; then
+  export NUM_GPU_PER_NODE=4
+  NODE_TYPE="v100"
+elif [[ "$SGE_RESOURCE_TYPE" == "rt_AF" ]]; then
+  export NUM_GPU_PER_NODE=8
+  NODE_TYPE="a100"
+else
+  echo "Unrecognized SGE_RESOURCE_TYPE: $SGE_RESOURCE_TYPE"
+fi
+
+NUM_NODES=$NHOSTS
+NUM_GPUS=$((${NUM_NODES} * ${NUM_GPU_PER_NODE}))
+
+mkdir -p ./hostfile
+
+HOSTFILE_NAME=./hostfile/hostfile_${JOB_ID}
+while read -r line; do
+  echo "${line} slots=${NUM_GPU_PER_NODE}"
+done <"$SGE_JOB_HOSTLIST" >"$HOSTFILE_NAME"
+
+
+# run
+mpirun -np $NUM_GPUS \
+  --npernode $NUM_GPU_PER_NODE \
+  -hostfile $HOSTFILE_NAME \
+  -x MASTER_ADDR=$MASTER_ADDR \
+  -x MASTER_PORT=$MASTER_PORT \
+  -x CUDA_DEVICE_MAX_CONNECTIONS=1 \
+  -bind-to none -map-by slot \
+  -x PATH \
+  python pretrain.py \
+    scripts/gpt-2/gpt-2-grad-shard.yaml
